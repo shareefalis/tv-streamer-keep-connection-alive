61c852ceea25b6607a6d08cfc6aaccccdf8ad007
Shareef Ali <shareefalis@gmail.com>
2020-12-14 06:38:22 -0500

    handle exceptions

--- audio.py
+++ audio.py
@@ -1,8 +1,14 @@
 import simpleaudio as sa
 import time
+import sys
 filename = 'audio.wav'
 wave_obj = sa.WaveObject.from_wave_file(filename)
 while(1):
-    play_obj = wave_obj.play()
-    play_obj.wait_done()  # Wait until sound has finished playing
-    time.sleep(250)
+    try:
+        play_obj = wave_obj.play()
+        play_obj.wait_done()  # Wait until sound has finished playing
+        time.sleep(250)
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except:
+        pass
